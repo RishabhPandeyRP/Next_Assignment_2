19a026bc64371d6b370901597cb1c6e6223665d6
RishabhPandeyRP <rishabh17704@gmail.com>
2025-02-22 18:30:32 +0530

    image prod issue

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ['www.theaudiostore.in' , 'https://encrypted-tbn1.gstatic.com' ], // Allow this domain for images
+    domains: ['www.theaudiostore.in' , 'encrypted-tbn1.gstatic.com' ], // Allow this domain for images
   },
 };
 
